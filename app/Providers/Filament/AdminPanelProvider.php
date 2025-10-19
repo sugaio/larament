b0c0085af3abc59f52f26cc98b8cfa72b103fd51
sugaio <sugairsorocnuk@gmail.com>
2025-10-19 20:10:26 +0700

    fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,11 +25,21 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->sidebarCollapsibleOnDesktop()
+                ->sidebarWidth('20rem')
+                ->collapsedSidebarWidth('5rem')
+            ->brandLogo(asset('images/logo.png'))
+                ->brandLogoHeight('3.0rem')
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
+            ->passwordReset()
+            ->emailVerification()
+            ->emailChangeVerification()
+            ->profile()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
